40c1ab45e3f250943fcf1b1623ebf403e44781dd
Antonio <AntonioRM81@Gmail.com>
2021-04-29 13:47:33 +0200

    Columnas ajustadas en tamaños de pantalla pequeños

--- pages/pokedex.jsx
+++ pages/pokedex.jsx
@@ -37,7 +37,7 @@ export default function Pokedex() {
     }
 
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
             {
                 pokemonQuery.data.map(pokemon =>
                     <div key={pokemon.id}>
