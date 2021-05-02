aabb75f05922f363dbfbe708328fa562d1bc269a
Antonio <AntonioRM81@Gmail.com>
2021-05-02 17:43:52 +0200

    Updated Next.js, created a custom Link component to trigger an activation class on current page, added a range filter to the WebDex, also added a Favicon and various other tweaks.

--- components/layouts/partials/Header.jsx
+++ components/layouts/partials/Header.jsx
@@ -1,10 +1,11 @@
 import Link from 'next/link'
+import CustomLink from '~/components/CustomLink'
 import Logo from './Logo'
 
 export default function Header() {
     return (
-        <header className="flex justify-center h-20 fixed top-0 inset-x-0">
-            <div className="container flex backdrop-filter backdrop-blur bg-transparent p-5">
+        <header className="flex justify-center h-20 fixed top-0 inset-x-0 backdrop-filter backdrop-blur bg-transparent z-10">
+            <div className="container flex p-5">
                 <Link href="/">
                     <a className="flex items-center space-x-3">
                         <Logo />
@@ -12,9 +13,9 @@ export default function Header() {
                     </a>
                 </Link>
                 <nav className="mr-auto ml-4 pl-4 border-l border-gray-400 flex items-center text-base">
-                    <Link href="/pokedex">
+                    <CustomLink href="/pokedex">
                         <a className="mr-5">Pokedex</a>
-                    </Link>
+                    </CustomLink>
                 </nav>
             </div>
         </header>
