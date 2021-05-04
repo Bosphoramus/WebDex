44152b5242c3ab443ee6f35328f7aa49f33b0e69
Antonio <AntonioRM81@Gmail.com>
2021-05-04 18:05:51 +0200

    Refactoring

---
+++ components/layouts/partials/ProjectPresentationAnimation.jsx
@@ -0,0 +1,11 @@
+import styles from '~/styles/presentationAnimation.module.css'
+
+export default function ProjectPresentationAnimation() {
+    return (
+        <div>
+            <div className={`${styles.container}`}>
+                <h1 className={`${styles.brandText}`}>WebDex</h1>
+            </div>
+        </div>
+    )
+}
